59502101a175bc343415ac995d989237e9a09d01
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2021-07-09 11:10:40 +0700

    add more classes

--- model/scripts/utils/config.py
+++ model/scripts/utils/config.py
@@ -1,5 +1,8 @@
 label_dict = {
     0: "cayxuongrong",
-    1: "cayhoado",
-    2: "cayhoavang",
+    1: "cayhoadonho",
+    2: "cayhoavanglon",
+    3: "cayhoatrang",
+    4: "cayhoavangnho",
+    5: "cayhoadolon",
 }
